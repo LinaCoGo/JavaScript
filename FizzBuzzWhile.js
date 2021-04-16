f7f27034f979595a3ef6f3a068a4a057d642d7e5
lina contreras <linacontreras@MacBook-Air-de-lina.local>
2021-04-16 14:45:20 -0500

    FizzBuzz excersise with a loop statement

---
+++ FizzBuzzWhile.js
@@ -0,0 +1,23 @@
+var fizzBuzzChallenge = [];
+var counter = 1;
+
+function fizzBuzz() {
+
+    while (counter <= 100) {
+    if (counter % 3 === 0 && counter % 5 === 0) {
+        fizzBuzzChallenge.push("FizzBuzz");
+    }
+    else if (counter % 3 === 0) {
+        fizzBuzzChallenge.push("Fizz");
+    }
+    else if (counter % 5 === 0) {
+        fizzBuzzChallenge.push("Buzz");
+    }
+    else {
+        fizzBuzzChallenge.push(counter);
+    }
+    counter++;
+    }
+}
+
+fizzBuzz();
